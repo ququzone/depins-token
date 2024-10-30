5a2403bfd9e836f09ff15feb571f8a8e011cdab9
ququzone <xueping.yang@gmail.com>
2024-10-30 17:19:00 +0800

    complete contract

---
+++ src/DepinsConvertor.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {MerkleProof} from "@openzeppelin-contracts/contracts/utils/cryptography/MerkleProof.sol";
+
+interface MintableToken {
+    function mint(address _user, uint256 _amount) external;
+}
+
+contract DepinsConvertor {
+    event Claim(address indexed account, uint256 amount);
+
+    uint256 public immutable foundationAmount;
+    bytes32 public root;
+    address public depins;
+    address public foundation;
+    mapping(address => bool) public claimed;
+
+    constructor(bytes32 _root, address _depins, address _foundation, uint256 _foundationAmount) {
+        root = _root;
+        depins = _depins;
+        foundation = _foundation;
+        foundationAmount = _foundationAmount;
+    }
+
+    function claimFoundation() external {
+        require(!claimed[foundation], "claimed");
+
+        claimed[foundation] = true;
+        MintableToken(depins).mint(foundation, foundationAmount);
+        emit Claim(foundation, foundationAmount);
+    }
+
+    function claim(address _account, uint256 _amount, bytes32[] calldata proof) external {
+        require(!claimed[_account], "claimed");
+        bytes32 node = keccak256(abi.encodePacked(_account, _amount));
+        require(MerkleProof.verify(proof, root, node), "invalid proof");
+
+        claimed[_account] = true;
+        MintableToken(depins).mint(_account, _amount);
+        emit Claim(_account, _amount);
+    }
+}
--- src/DepinsToken.sol
+++ src/DepinsToken.sol
@@ -1,5 +1,27 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-contract DepinsToken {
+import {ERC20} from "@openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+
+contract DepinsToken is ERC20 {
+    event ChangeMinter(address indexed minter);
+
+    address public minter;
+
+    constructor() ERC20("Depins", "DEPINS") {
+        minter = msg.sender;
+    }
+
+    function changeMinter(address _minter) external {
+        require(msg.sender == minter, "only minter");
+
+        minter = _minter;
+        emit ChangeMinter(_minter);
+    }
+
+    function mint(address _user, uint256 _amount) external {
+        require(msg.sender == minter, "only minter");
+
+        _mint(_user, _amount);
+    }
 }
